001bf1bf5fceda072f66f43f4c10e9db8ca68667
kIvy1189 <kevindivy@gmail.com>
2020-06-20 18:56:51 -0500

    Added comment routes and tested functionality

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+//Get all comments
+router.get('/', (req, res) => {
+    console.log(`=====================`);
+    Comment.findAll({
+        attributes: [
+            'id',
+            'comment_text',
+            'user_id',
+            'post_id'
+        ],
+        order: [['created_at', 'DESC']]
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Create new comments
+router.post('/', (req, res) => {
+    // check the session
+    if (req.session) {
+      Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
+
+//Delete individual comments
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(400).json({message: 'No post found with this id'});
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 //create route paths
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
 
 //call route paths
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
